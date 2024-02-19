830bcab17239fe809db5f671b1d29a5f3f780e43
Lutecindiateam <mahesh.raut@lutecindia.com>
2024-02-19 14:36:34 +0530

    dev msg

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,19 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "prode8a4f459": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3033e72d8": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
